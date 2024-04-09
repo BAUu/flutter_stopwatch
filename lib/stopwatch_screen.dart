b4d611c95f4d14d6ad44ca5bc0779842722cc9c9
BAUu <yimej07@gmail.com>
2024-04-09 17:44:23 +0900

    stopwatch

---
+++ lib/stopwatch_screen.dart
@@ -0,0 +1,139 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class StopWatchScreen extends StatefulWidget {
+  const StopWatchScreen({super.key});
+
+  @override
+  State<StopWatchScreen> createState() => _StopWatchScreenState();
+}
+
+class _StopWatchScreenState extends State<StopWatchScreen> {
+  Timer? _timer;
+  int _time = 0;
+  bool _isRunning = false;
+
+  final List<String> _lapTime = [];
+
+  void _clickButton() {
+    _isRunning = !_isRunning;
+    if (_isRunning) {
+      _start();
+    } else {
+      _pause();
+    }
+  }
+
+  void _start() {
+    _timer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
+      setState(() {
+        _time++;
+      });
+    });
+  }
+
+  void _pause() {
+    _timer?.cancel();
+  }
+
+  void _reset() {
+    _isRunning = false;
+    _timer?.cancel();
+    _lapTime.clear();
+    _time = 0;
+  }
+
+  void _recordLapTIme(String time) {
+    _lapTime.insert(0, '${_lapTime.length + 1}등 $time');
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int sec = _time ~/ 100;
+    String hundredth = '${_time % 100}'.padLeft(2, '0');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('스톱워치'),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 32,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                '$sec',
+                style: const TextStyle(fontSize: 50, color: Colors.orangeAccent),
+              ),
+              Text(
+                hundredth,
+                style: const TextStyle(fontSize: 22, color: Colors.orange),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 100,
+            height: 200,
+            child: ListView(
+              children: _lapTime
+                  .map((time) => Center(
+                          child: Text(
+                        time,
+                        style: const TextStyle(fontSize: 16),
+                      )))
+                  .toList(),
+            ),
+          ),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                backgroundColor: Colors.red,
+                onPressed: () {
+                  setState(() {
+                    _reset();
+                  });
+                },
+                child: const Icon(Icons.refresh),
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.blue,
+                onPressed: () {
+                  setState(() {
+                    _clickButton();
+                  });
+                },
+                child: _isRunning
+                    ? const Icon(Icons.pause)
+                    : const Icon(Icons.play_arrow),
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.yellow,
+                onPressed: () {
+                  setState(() {
+                    _recordLapTIme('$sec.$hundredth');
+                  });
+                },
+                child: const Icon(Icons.add),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+        ],
+      ),
+    );
+  }
+}
